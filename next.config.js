678eadb375fea593b04577868340cc883a3def1a
ghrp8277 <ghrp8277>
2023-04-01 19:58:29 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
 
 module.exports = async (phase, { defaultConfig }) => {
-  const config = require('dotenv').config('.env')
+  const config = require("dotenv").config(".env");
 
-  const parsed = config.parsed
+  const parsed = config.parsed;
 
   const nextConfig = {
     reactStrictMode: true,
@@ -13,8 +13,15 @@ module.exports = async (phase, { defaultConfig }) => {
     },
     env: {
       appConfig: parsed,
-      customKey: 'my-value',
-    }
-  }
-  return nextConfig
-}
+      customKey: "my-value",
+    },
+    serverRuntimeConfig: {
+      mySecret: "secret",
+      secondSecret: process.env.SECOND_SECRET,
+    },
+    publicRuntimeConfig: {
+      staticFolder: "/static",
+    },
+  };
+  return nextConfig;
+};
